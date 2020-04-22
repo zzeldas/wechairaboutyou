abda581234305a16bb5639be570f062a857f308a
Liang Liang <lilysway@hotmail.com>
2020-04-22 13:01:52 -0400

    code review 1 fix user model & user route security middleware isAdmin & isLoggedIn

--- server/api/users.js
+++ server/api/users.js
@@ -3,18 +3,28 @@ const User = require('../db/models/user')
 const Order = require('../db/models/order')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+//middleware isAdmin
+const isAdmin = (req, res, next) => {
+  //check req.user.isAdmin is true
+  if (!req.user || !req.user.isAdmin) {
+    res.status(401).json({error: 'You are not authorized'})
+  }
+  next()
+}
+//middleware isLoggedIn
+const isLoggedIn = (req, res, next) => {
+  //check req.session.userId is there
+  if (!req.session.userId) {
+    res.status(401).json({error: 'You are not logged in'})
+    //redirect to log in page
+  }
+  next()
+}
+//get all users route
+router.get('/', isAdmin, async (req, res, next) => {
   try {
     const users = await User.findAll({
-      attributes: [
-        'id',
-        'name',
-        'email',
-        'loginId',
-        'address',
-        'phoneNum',
-        'isAdmin'
-      ]
+      attributes: ['id', 'firstName', 'lastName', 'email', 'address', 'isAdmin']
     })
     res.json(users)
   } catch (err) {
@@ -22,7 +32,8 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-router.get('/:userId', async (req, res, next) => {
+//get single User route
+router.get('/:userId', isLoggedIn, async (req, res, next) => {
   try {
     const userId = req.params.userId
     const foundUser = await User.findByPk(userId, {include: [{model: Order}]})
@@ -31,3 +42,9 @@ router.get('/:userId', async (req, res, next) => {
     next(err)
   }
 })
+
+//add single user route
+
+//update single user route for admin and login user (may need two routes )
+
+//delete single user route
