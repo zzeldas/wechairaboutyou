01c7bf85a4c0d091e8d8f847db90995eea9215d2
yr.min17@gmail.com <Lily@aggieui-macbook.home>
2020-04-22 20:07:20 -0400

    add allProducts reducer

--- client/store/combineReducers.js
+++ client/store/combineReducers.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux'
 import userReducer from './singleUser'
 import usersReducer from './users'
+import productsReducer from './products'
 
 const appReducer = combineReducers({
   users: usersReducer,
-  user: userReducer
+  user: userReducer,
+  products: productsReducer
 })
 
 export default appReducer
---
+++ client/store/products.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+import history from '../history'
+
+/**
+ * ACTION TYPES
+ */
+const GET_ALL_PRODUCTS = 'GET_ALL_PRODUCTS'
+
+/**
+ * INITIAL STATE
+ */
+const initialState = []
+
+/**
+ * ACTION CREATORS
+ */
+const getAllProducts = allProducts => ({
+  type: GET_ALL_PRODUCTS,
+  allProducts
+})
+
+/**
+ * THUNK CREATORS
+ */
+export const fetchAllProducts = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/products')
+    dispatch(getAllProducts(res.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function productsReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_ALL_PRODUCTS:
+      return [...state, action.allProducts]
+    default:
+      return state
+  }
+}
