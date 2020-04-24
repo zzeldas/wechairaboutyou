ce88c3197200a3b8d6068b6478a2c7b6a6093e0c
Liang Liang <lilysway@hotmail.com>
2020-04-23 23:22:16 -0400

    change cart api to go into the products routes

--- server/api/cart.js
+++ server/api/cart.js
@@ -9,9 +9,9 @@ router.get('/', async (req, res, next) => {
   //Check if there is a pending cart in orders
   //if pending cart, it goes to cart id
   //else create pending order
-
   try {
     //----
+
     let cart = await Order.findAll({
       where: {
         status: 'pending'
@@ -26,7 +26,28 @@ router.get('/', async (req, res, next) => {
       res.json(cart)
     }
   } catch (err) {
-    console.error(err)
+    next(err)
+  }
+})
+
+//user adds item to cart
+//url /:userId/cart
+router.post('/', async (req, res, next) => {
+  try {
+    const addedProduct = await OrderProduct.create()
+    //check for pending order
+    let {orders} = await User.findByPk(req.params.userId, {
+      include: [{model: Order}]
+    })
+
+    //if there is one pending order
+    //look up pending order's id
+    //assign addedProduct's orderId to the id of the pending order
+    //if there is no pending order
+    //let newCart = Order.create()
+    //assign addedProduct's orderId to the if of the newCart
+    //res.json(newCart)
+  } catch (err) {
     next(err)
   }
 })
