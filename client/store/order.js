626799f8623b3e48646c600e66fcb04309d17449
Arielle Domantay <domantayarielle@gmail.com>
2020-04-24 23:42:11 -0400

    adding a order reducer

--- client/store/order.js
+++ client/store/order.js
@@ -5,6 +5,7 @@ import history from '../history'
  * ACTION TYPES
  */
 const GET_ALL_ORDERS = 'GET_ALL_ORDERS'
+const GET_PENDING_ORDER = 'GET_PENDING_ORDER'
 const ADD_ORDER = 'ADD_ORDER'
 
 /**
@@ -27,7 +28,7 @@ const addUser = newUser => ({
 /**
  * THUNK CREATORS
  */
-export const fetchAllUsers = () => async dispatch => {
+export const fetchAllOrders = () => async dispatch => {
   try {
     const res = await axios.get('/api/users')
 
@@ -37,27 +38,19 @@ export const fetchAllUsers = () => async dispatch => {
   }
 }
 
-export const fetchSignUp = () => async dispatch => {
+export const fetchPendingOrder = () => async dispatch => {
   try {
-    const res = await axios.post('/api/users', {
-      firstName,
-      lastName,
-      email,
-      password,
-      address
-    })
-    dispatch(addUser(res.data))
+    const res = await axios.get('/api/users')
+
+    dispatch(getAllUsers(res.data))
   } catch (err) {
     console.error(err)
   }
 }
 
-/**
- * REDUCER
- */
-export default function usersReducer(state = initialState, action) {
+export default function ordersReducer(state = initialState, action) {
   switch (action.type) {
-    case GET_ALL_USERS:
+    case GET_ALL_ORDERS:
       return action.allUsers
     case ADD_USER:
       return [...state, action.newUser]
