00e937b5b43b741fd9d6578ed461d0464016d3b0
yr.min17@gmail.com <Lily@aggieui-macbook.home>
2020-04-28 11:51:46 -0400

    test touch up

--- server/db/models/orderproduct.spec.js
+++ server/db/models/orderproduct.spec.js
@@ -1,32 +1,32 @@
-const {expect} = require('chai')
-const db = require('../index')
-const Product = require('../models/orderproduct')
+// const {expect} = require('chai')
+// const db = require('../index')
+// const Product = require('../models/orderproduct')
 
-describe('Order Product model', () => {
-  beforeEach(() => {
-    return db.sync({force: true})
-  })
+// describe('Order Product model', () => {
+//   beforeEach(() => {
+//     return db.sync({force: true})
+//   })
 
-  describe('instanceMethods', () => {
-    describe('data', () => {
-      let product
-      let orderProduct
+//   describe('instanceMethods', () => {
+//     describe('data', () => {
+//       let product
+//       let orderProduct
 
-      beforeEach(async () => {
-        product = await Product.create({
-          price: 129.99,
-          quantity: 5
-        })
-        orderProduct = {
-          product: product
-        }
-      })
+//       beforeEach(async () => {
+//         product = await Product.create({
+//           price: 129.99,
+//           quantity: 5
+//         })
+//         orderProduct = {
+//           product: product
+//         }
+//       })
 
-      it('it has all fields: price, quantity, and total', () => {
-        expect(orderProduct.product.price).to.equal(12999)
-        expect(orderProduct.product.quantity).to.equal('Gaming')
-        expect(orderProduct.product.total).to.equal(64995)
-      })
-    }) // end describe('correctPassword')
-  }) // end describe('instanceMethods')
-}) // end describe('User model')
+//       it('it has all fields: price, quantity, and total', () => {
+//         expect(orderProduct.product.price).to.equal(12999)
+//         expect(orderProduct.product.quantity).to.equal('Gaming')
+//         expect(orderProduct.product.total).to.equal(64995)
+//       })
+//     }) // end describe('correctPassword')
+//   }) // end describe('instanceMethods')
+// }) // end describe('User model')
--- server/db/models/products.spec.js
+++ server/db/models/products.spec.js
@@ -1,73 +1,73 @@
-const {expect} = require('chai')
-const db = require('../index')
-const Product = require('../models/product')
+// const {expect} = require('chai')
+// const db = require('../index')
+// const Product = require('../models/product')
 
-describe('Product model', () => {
-  beforeEach(() => {
-    return db.sync({force: true})
-  })
+// describe('Product model', () => {
+//   beforeEach(() => {
+//     return db.sync({force: true})
+//   })
 
-  describe('instanceMethods', () => {
-    describe('correctField', () => {
-      let product
+//   describe('instanceMethods', () => {
+//     describe('correctField', () => {
+//       let product
 
-      beforeEach(async () => {
-        product = await Product.create({
-          name: 'Devoko',
-          price: 129.99,
-          category: 'Gaming',
-          description:
-            'Devoko high back ergonomic gaming chair covered by premium breathable leather and freely adjustable lumbar support & headrest pillow protecting your spinal & neck.',
-          color: 'Red',
-          size: 'Large',
-          imageUrl:
-            'https://images-na.ssl-images-amazon.com/images/I/71iD3uTLZ0L._AC_SL1500_.jpg',
-          quantity: 7
-        })
-      })
+//       beforeEach(async () => {
+//         product = await Product.create({
+//           name: 'Devoko',
+//           price: 129.99,
+//           category: 'Gaming',
+//           description:
+//             'Devoko high back ergonomic gaming chair covered by premium breathable leather and freely adjustable lumbar support & headrest pillow protecting your spinal & neck.',
+//           color: 'Red',
+//           size: 'Large',
+//           imageUrl:
+//             'https://images-na.ssl-images-amazon.com/images/I/71iD3uTLZ0L._AC_SL1500_.jpg',
+//           quantity: 7
+//         })
+//       })
 
-      it('it has all field: name, price, category, description, color, size, imageUrl, quantity', () => {
-        expect(product.name).to.equal('Devoko')
-        expect(product.price).to.equal(12999)
-        expect(product.category).to.equal('Gaming')
-        expect(product.description).to.equal(
-          'Devoko high back ergonomic gaming chair covered by premium breathable leather and freely adjustable lumbar support & headrest pillow protecting your spinal & neck.'
-        )
-        expect(product.color).to.equal('Red')
-        expect(product.size).to.equal('Large')
-        expect(product.imageUrl).to.equal(
-          'https://images-na.ssl-images-amazon.com/images/I/71iD3uTLZ0L._AC_SL1500_.jpg'
-        )
-        expect(product.quantity).to.equal(7)
-      })
-    }) // end describe('correctPassword')
-  }) // end describe('instanceMethods')
-}) // end describe('User model')
+//       it('it has all field: name, price, category, description, color, size, imageUrl, quantity', () => {
+//         expect(product.name).to.equal('Devoko')
+//         expect(product.price).to.equal(12999)
+//         expect(product.category).to.equal('Gaming')
+//         expect(product.description).to.equal(
+//           'Devoko high back ergonomic gaming chair covered by premium breathable leather and freely adjustable lumbar support & headrest pillow protecting your spinal & neck.'
+//         )
+//         expect(product.color).to.equal('Red')
+//         expect(product.size).to.equal('Large')
+//         expect(product.imageUrl).to.equal(
+//           'https://images-na.ssl-images-amazon.com/images/I/71iD3uTLZ0L._AC_SL1500_.jpg'
+//         )
+//         expect(product.quantity).to.equal(7)
+//       })
+//     }) // end describe('correctPassword')
+//   }) // end describe('instanceMethods')
+// }) // end describe('User model')
 
-// describe("Sequelize Model", () => {
-//     before(() => db.sync({ force: true }));
-//     afterEach(() => db.sync({ force: true }));
+// // describe("Sequelize Model", () => {
+// //     before(() => db.sync({ force: true }));
+// //     afterEach(() => db.sync({ force: true }));
 
-//     xit("it returns correct fields name, description, category, imageUrl, price, size, color, quantity", async () => {
-//       const product = await Product.create({
-//         name: "Devoko",
-//         price: 129.99,
-//         category: "Gaming",
-//         description: "Devoko high back ergonomic gaming chair covered by premium breathable leather and freely adjustable lumbar support & headrest pillow protecting your spinal & neck.",
-//         color: "Red",
-//         size: "Large",
-//         imageUrl: "https://images-na.ssl-images-amazon.com/images/I/71iD3uTLZ0L._AC_SL1500_.jpg",
-//         quantity: 7
-//       });
+// //     xit("it returns correct fields name, description, category, imageUrl, price, size, color, quantity", async () => {
+// //       const product = await Product.create({
+// //         name: "Devoko",
+// //         price: 129.99,
+// //         category: "Gaming",
+// //         description: "Devoko high back ergonomic gaming chair covered by premium breathable leather and freely adjustable lumbar support & headrest pillow protecting your spinal & neck.",
+// //         color: "Red",
+// //         size: "Large",
+// //         imageUrl: "https://images-na.ssl-images-amazon.com/images/I/71iD3uTLZ0L._AC_SL1500_.jpg",
+// //         quantity: 7
+// //       });
 
-//       expect(product.name).to.equal("Devoko");
-//       expect(product.price).to.equal(129.99);
-//       expect(product.category).to.equal("Gaming");
-//       expect(product.description).to.equal("Devoko high back ergonomic gaming chair covered by premium breathable leather and freely adjustable lumbar support & headrest pillow protecting your spinal & neck."
-//       );
-//       expect(product.color).to.equal("Red");
-//       expect(product.size).to.equal("Large");
-//       expect(product.imageUrl).to.equal("/images/jupiter.png");
-//       expect(product.quantity).to.equal(7);
-//     });
-// });
+// //       expect(product.name).to.equal("Devoko");
+// //       expect(product.price).to.equal(129.99);
+// //       expect(product.category).to.equal("Gaming");
+// //       expect(product.description).to.equal("Devoko high back ergonomic gaming chair covered by premium breathable leather and freely adjustable lumbar support & headrest pillow protecting your spinal & neck."
+// //       );
+// //       expect(product.color).to.equal("Red");
+// //       expect(product.size).to.equal("Large");
+// //       expect(product.imageUrl).to.equal("/images/jupiter.png");
+// //       expect(product.quantity).to.equal(7);
+// //     });
+// // });
